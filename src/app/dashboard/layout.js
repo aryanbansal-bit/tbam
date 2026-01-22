bf8c772c8c3007e1087dca35d8fd1c55f51418d9
aryan <aryan.bansal@ibotsystems.com>
2026-01-22 20:27:03 +0530

    corrections

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google';
-import '@/globals.css';
-import Navbar from '@/components/Navbar';
+import '@/app/globals.css';
+import Navbar from '@/app/components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
