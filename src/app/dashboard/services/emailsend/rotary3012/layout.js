db53547bc35134912f76439e69a0a6b4e29b38b4
aryan <aryan.bansal@ibotsystems.com>
2026-01-27 00:48:21 +0530

    update

---
+++ src/app/dashboard/services/emailsend/rotary3012/layout.js
@@ -0,0 +1,21 @@
+import { Inter } from 'next/font/google';
+import '@/app/globals.css';
+import Navbar from '@/app/components/Navbar-service';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'Rotary Management',
+  description: 'Rotary Club Management System',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
